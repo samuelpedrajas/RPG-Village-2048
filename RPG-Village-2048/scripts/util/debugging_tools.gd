95723385308cb5b35d45b782f9b5e7b64d458658
Samuel Pedrajas <samuel.p991@gmail.com>
2017-08-04 16:13:42 +0100

    resized sprites and debugging script

---
+++ RPG-Village-2048/scripts/util/debugging_tools.gd
@@ -0,0 +1,26 @@
+extends Node
+
+const DEFAULT_PATH = "res://scripts/util/logs/"
+
+func _dump_dict_recursive(d, s, t):
+	s += "{\n"
+	t += "\t"
+	for key in d.keys():
+		var value = d[key]
+		s += t + str(key) + ": "
+		if (typeof(value) == TYPE_DICTIONARY):
+			s = _dump_dict_recursive(value, s, t + "\t") + t + "},\n"
+		else:
+			s += str(value) + ",\n"
+	return s
+
+func _dump_to_file(s, path):
+	var f = File.new()
+
+	f.open(path, f.WRITE)
+	f.store_string(s)
+	f.close()
+
+func _dump_dict(d, fn):
+	var output = _dump_dict_recursive(d, "", "")
+	_dump_to_file(output, DEFAULT_PATH + fn + ".log")
