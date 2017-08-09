354a64973349a3a5712caf96a6183bc0b257ec57
Samuel Pedrajas <samuel.p991@gmail.com>
2017-08-09 01:36:13 +0100

    importing sprites fixed

--- RPG-Village-2048/scripts/util/textures_to_sprites.gd
+++ RPG-Village-2048/scripts/util/textures_to_sprites.gd
@@ -9,6 +9,7 @@ var current_pos = Vector2(0, 0)
 var h_spacing = 16
 var v_spacing = 512
 
+var current_layer = 2
 
 func _get_full_path(categories):
 	var path = TILES_PATH
@@ -20,7 +21,7 @@ func _get_full_path(categories):
 
 
 func _get_file_name(file_path):
-	var file_name = file_path.substr(0, file_path.length() - 1)
+	var file_name = file_path.substr(0, file_path.length())
 
 	return file_name.replace(TILES_PATH, "").replace("/", "-").replace(".tex", "")
 
@@ -35,7 +36,7 @@ func _recursive_import(dir_path, categories):
 	name = d.get_next()
 
 	while name != "":
-		if name in [".", ".."]:
+		if name in [".", ".."] or name.ends_with(".json"):
 			name = d.get_next()
 			continue
 
@@ -43,7 +44,7 @@ func _recursive_import(dir_path, categories):
 			var new_dir_path = _get_full_path(categories + [name])
 			_recursive_import(new_dir_path, categories + [name])
 		else:
-			var file_path = _get_full_path(categories + [name])
+			var file_path = _get_full_path(categories) + name
 			_create_sprite(file_path)
 		
 		name = d.get_next()
@@ -51,14 +52,25 @@ func _recursive_import(dir_path, categories):
 func _run():
 	_recursive_import(TILES_PATH, [])
 
+func _load_json(file_path):
+	var d = {}
+	var file = File.new()
+	file.open(file_path, file.READ)
+	d.parse_json(file.get_as_text())
+	file.close()
+	return d
+
 func _create_sprite(file_path):
+	var info = _load_json(file_path.replace(".tex", ".json"))
+	if info.layer != current_layer:
+		return
 	var s = Sprite.new()
 	var t = ResourceLoader.load(file_path)
 
 	s.set_texture(t)
 	s.set_name(_get_file_name(file_path))
 	s.set_pos(current_pos)
-	# s.set_offset(Vector2((CELL_SIZE.x - t.get_width()) / 2, (CELL_SIZE.y - t.get_height()) / 2))
+	s.set_offset(Vector2(info.offset.x, info.offset.y))
 	get_scene().add_child(s)
 	s.set_owner(get_scene())
 	current_pos.x += t.get_width() + h_spacing
